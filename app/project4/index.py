ce0ab687f75e60f3a31e348e7c622d6132220aca
Marc <phyrenight@gmail.com>
2016-03-30 15:44:21 -0400

    added hiddenWord()

--- app/project4/index.py
+++ app/project4/index.py
@@ -2,6 +2,9 @@ import endpoints
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
+from random import randint
+
+lst = ["cat", "carp", "king"]
 
 REQUEST_LETTER = endpoints.ResourceContainer(
 	message_types.VoidMessage,
@@ -10,17 +13,20 @@ REQUEST_LETTER = endpoints.ResourceContainer(
 REQUEST_WORD = endpoints.ResourceContainer(
 	name = messages.StringField(1),
 )
-package = "response"
+
+REQUEST_NEW_GAME = endpoints.ResourceContainer(
+    name = messages.StringField(1),
+	)
 
 class Response(messages.Message):
     response = messages.StringField(1)
 
-@endpoints.api(name = 'hangmanEndPoints', version='1')
-class hangmanEndPoints(remote.Service):
+@endpoints.api(name='hangmanEndPoints', version='v1')
+class hangmanApi(remote.Service):
 
     @endpoints.method(REQUEST_LETTER, Response, path = "letter_given", http_method = "GET", name = "letter")
-    def letterGiven(self, choice):
-    	letter = "{}".format(choice.name)
+    def letterGiven(self, request):
+    	letter = "{}".format(request.name)
         return Response(response=letter)
 
     @endpoints.method(REQUEST_WORD, Response, path="word_given", http_method="GET", name="user_word")
@@ -28,7 +34,21 @@ class hangmanEndPoints(remote.Service):
     	words = "{}".format(choice.name)
     	return Response(response=words)
 
+    @endpoints.method(message_types.VoidMessage, Response, path="new_game", http_method="Get", name="new_game")
+    def newGame(self, choice):
+        """ starts the game
+        returns the hiddenWord
+        """
+        word = getWord()
+        #hiddenWord = hideWord(word)
+        return Response(response=word)
 
 
+def getWord():
+	length = len(lst)
+	num = randint(0,length)
+	word = lst[num]
+	return word
 
-APPLICATION = endpoints.api_server([hangmanEndPoints])
+
+APPLICATION = endpoints.api_server([hangmanApi])
